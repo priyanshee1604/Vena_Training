a584b827613bc26f63bbe7746f978d4a7a2c1953
priyanshee1604 <priyansheesisodiya1604@gmail.com>
2025-04-25 13:17:38 +0530

    Implemented: Question6, Student Registry, Static vs Instance

---
+++ JAVA/OOPS/com/priyanshee/dayone/question6/Main.java
@@ -0,0 +1,17 @@
+package com.priyanshee.dayone.question6;
+
+public class Main {
+    public static void main(String[] args) {
+        Student st1 = new Student("Priyanshee", 1);
+        st1.studentDetail();
+        st1.totalCountDisplay();
+
+        Student st2 = new Student("Priyansh", 2);
+        st2.studentDetail();
+        st2.totalCountDisplay();
+
+        Student st3 = new Student("Priyanshu", 3);
+        st3.studentDetail();
+        st3.totalCountDisplay();
+    }
+}
---
+++ JAVA/OOPS/com/priyanshee/dayone/question6/Student.java
@@ -0,0 +1,22 @@
+package com.priyanshee.dayone.question6;
+
+public class Student {
+    private static int totalStudents = 0;
+    private int id;
+    private String name;
+
+    public  Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+        totalStudents++;
+    }
+
+    public void totalCountDisplay() {
+        System.out.println("The total number of students / objects are " + totalStudents);
+    }
+
+    public void studentDetail() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+    }
+}
